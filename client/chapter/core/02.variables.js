94394b7d0eb814bbf13dbbfca4d7d6df662833ba
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-05-29 15:51:50 +0900

    feat:5/29 수업

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,63 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+//병수 가지고 놀기
+let admin;
+let name ;
+name ='john';
+admin = name;
+console.log(admin);
+
+// 올바른 이름 선택하기
+const OUR_PLANET_NAME ='EARTH';
+let currentOnlineUserName = 'EARTH';
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calc_purchase_price ;
+// - 구매 제품 가격의 총 합
+let total_sum_purchase_price = 230;
+// - 1년 기준 일(day)자 수
+const DAY_OF_YEAR = 365;
+// - 구매 결제 여부
+// const isPayment = true; 
+// - 구매 결제 내역
+const purchase_reciept = 'anj';
+// - 브랜드 접두사
+// const BRAND_PREFIX = 'dd';
+// - 오늘의 운세
+const today_luck = '90';
+// - 상품 정보
+const product_information = 'good';
+
+
+/* variables ----------------------------------------------------------- */
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceQuentity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice ;
+
+// - 구매 결제 여부
+let isPayment = true;
+
+// - 구매 결제 내역
+let paymentList;
+
+// - 오늘의 운세
+let fotfuneToday;
+
+/* constant variables -------------------------------------------------- */
+
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = 365;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'NIKE';
+
+// - 상품 정보
+const product_info = 'good';
+
