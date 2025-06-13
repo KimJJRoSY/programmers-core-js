327bcf994630ccd80f158cc53a7e688b30e0f7b9
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-06-13 09:01:38 +0900

    feat: 6/12

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node, ...types },
+      globals: { ...globals.browser, ...globals.node, ...types, getNode: true },
     },
     rules: {
       'no-unused-vars': 'off',
